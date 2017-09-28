1cd91782f274661aaf74e8a0e31f1e842c4d5ff6
Viktor Georgiev <v.georgiev90@abv.bg>
2017-09-28 10:47:41 +0300

    Update mysql-replication.sh

--- mysql-replication.sh
+++ mysql-replication.sh
@@ -11,6 +11,29 @@ mysql-install () {
   systemctl start mariadb
   systemctl enable mariadb
   /usr/bin/mysqladmin -u root password 'viktor123'
+cat > /etc/my.cnf << EOF
+[mysqld]
+datadir=/var/lib/mysql
+socket=/var/lib/mysql/mysql.sock
+
+
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+# Settings user and group are ignored when systemd is used.
+# If you need to run mysqld under a different user or group,
+# customize your systemd unit file for mariadb according to the
+# instructions in http://fedoraproject.org/wiki/Systemd
+
+[mysqld_safe]
+log-error=/var/log/mariadb/mariadb.log
+pid-file=/var/run/mariadb/mariadb.pid
+
+#
+# include all files from the config directory
+#
+!includedir /etc/my.cnf.d
+EOF
+
 }
 
 mysql-master-part () {
@@ -25,18 +48,24 @@ EOF
   read DB
   echo "Provide id number of the server (1,2): "
   read ID
+  echo "Provide the remote address of the other host: "
+  read HOST
   INS=`grep -n "socket=/var/lib/mysql/mysql.sock" /etc/my.cnf | awk -F":" '{print $1}'`
   LINE=$(expr $INS + 1)
-  sed -i '$LINEi server-id  = $ID' /etc/my.cnf
-  sed -i '$LINEi log_bin = /var/log/mariadb/mariadb-bin.log' /etc/my.cnf
-  sed -i "$LINEi binlog_do_db = $DB" /etc/my.cnf
-  systemctl restart mariadb
-  echo "create user 'replica'@'%' identified by 'parolata';" | mysql
-  echo "grant replication slave on *.* to 'replica'@'%';"
-  echo "These are master log file and log position needed to set up the slave part"
-  echo "=========================================================================="
-  echo "show master status;" | mysql | awk -F" " '{print $1}' | tail -1`
-  echo "show master status;" | mysql | awk -F" " '{print $2}' | tail -1`
+  if [ -f "/etc/my.cnf" ]; then
+    sed -i "${LINE}i server-id  = ${ID}" /etc/my.cnf
+    sed -i "${LINE}i log_bin = /var/log/mariadb/mariadb-bin.log" /etc/my.cnf
+    sed -i "${LINE}i binlog_do_db = ${DB}" /etc/my.cnf
+    systemctl restart mariadb
+    echo "create user 'replica'@'${HOST}' identified by 'parolata';" | mysql
+    echo "grant replication slave on *.* to 'replica'@'${HOST}';"
+    echo "These are master log file and log position needed to set up the slave part"
+    echo "=========================================================================="
+    echo "show master status;" | mysql | awk -F" " '{print $1}' | tail -1
+    echo "show master status;" | mysql | awk -F" " '{print $2}' | tail -1
+  else
+    echo "There is no my.cnf file please check and add one."
+  fi
 }
 
 mysql-slave-part () {
