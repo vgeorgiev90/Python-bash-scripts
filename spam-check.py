50f212099dfc087b8afb64a5d8e7022ff856e8e4
Viktor Georgiev <v.georgiev90@abv.bg>
2017-11-17 09:05:43 +0200

    Update spam-check.py

--- spam-check.py
+++ spam-check.py
@@ -11,11 +11,16 @@ import collections
 def queue_filter():
 
     proc = subprocess.Popen(['exim', '-bp'], stdout=subprocess.PIPE)
-    word = proc.stdout.read()                               
+    word = proc.stdout.read()
 
     #regex patterns for mail account and user account
 
+    #mail accounts example@example.com
+
     match = re.findall('([a-zA-Z0-9_.-]{2,}@[a-zA-Z0-9-]{2,}.[a-zA-Z0-9]{2,}.[a-zA-Z0-9]{2,})', word)
+
+    #user accounts (username)
+
     match2 = re.findall('(\([a-zA-Z0-9_.-]{2,}\))', word)
 
     spam = []
@@ -25,68 +30,80 @@ def queue_filter():
         counter += 1
     spam2 = []
     for row in match2:
-	counter2 = 0
-	spam2.insert(counter2, row)
-	counter2 += 1
+        counter2 = 0
+        spam2.insert(counter2, row)
+        counter2 += 1
 
-    global fst, scd, trd, frd
-    global fst2, scd2, trd2
+    global fst, scd
+    global fst2, scd2
     count = collections.Counter(spam).most_common(5)
     count2 = collections.Counter(spam2).most_common(5)
     try:
-        print("Possible account sources...")
-        print("===========================")
-        fst2, scd2, trd2 = str(count2[0]), str(count2[1]), str(count2[2])
-        print(fst2.split(",")[0] + "    =====>    " + fst2.split(",")[1])
-        print(scd2.split(",")[0] + "    =====>    " + scd2.split(",")[1])
-        print(trd2.split(",")[0] + "    =====>    " + trd2.split(",")[1])
-        print("")
+        fst2, scd2 = str(count2[0]), str(count2[1])
     except IndexError:
-	try:
-	    print("Only one possible account...")
-	    fst2 = str(count2[0])
-	    print(fst2.split(",")[0] + "    =====>    " + fst2.split(",")[1])
-	    print("")
-	except IndexError:
-	    print("No spam scripts check mail accounts...")
+        try:
+            fst2 = str(count2[0])
+        except IndexError:
+            print("No spam scripts check mail accounts...")
     try:
-        print("Possible mail sources...")
-        print("===========================")
-        fst, scd, trd, frd = str(count[0]), str(count[1]), str(count[2]), str(count[3])
-        print(fst.split(",")[0] + "    =====>    " + fst.split(",")[1])
-        print(scd.split(",")[0] + "    =====>    " + scd.split(",")[1])
-        print(trd.split(",")[0] + "    =====>    " + trd.split(",")[1])
-        print(frd.split(",")[0] + "    =====>    " + frd.split(",")[1])
-        print("")
+        fst, scd = str(count[0]), str(count[1])
     except IndexError:
-	try:
-	    print("Only one possible account...")
+        try:
             fst = str(count[0])
-            print(fst.split(",")[0] + "    =====>    " + fst.split(",")[1])
-            print("")
-	except IndexError:
-	    print("No mail queue or something is wrong...")
+        except IndexError:
+            print("No mail queue or something is wrong...")
 
 
-def vivaweb_check():
-    user1 = re.findall('@[a-zA-Z0-9-]{2,}.vivawebhost.com', fst)
-    user2 = re.findall('@[a-zA-Z0-9-]{2,}.vivawebhost.com', scd)
-    global acc1, acc2
-    temp1 = fst.split("@")[0]
-    temp2 = scd.split("@")[0]
-    acc1 = temp1.split("'")[1]
-    acc2 = temp2.split("'")[1]
+def vivaweb_check(user):
+    regex = re.findall('@[a-zA-Z0-9-]{2,}.vivawebhost.com', user)
+    if regex:
+        account = user.split("@")[0]
+        return account
 
-    if user1 or user2:
-	print("Check the following accounts.. ")
-	print(acc1 + ",     " + acc2)
-    elif user1:
-	print(acc1)
-    elif user2:
-	print(acc2)
+def mail_strip(user):
+    tmp = user.split(",")[0]
+    tmp2 = tmp.split("'")[1]
+    account = tmp2.split("'")[0]
+    count = user.split(",")[1]
+    count2 = count.split(")")[0]
+    return account, count2
+
+def acc_strip(user):
+    tmp = user.split("'")[1]
+    tmp2 = tmp.split("(")[1]
+    account = tmp2.split(")")[0]
+    count = user.split(",")[1]
+    count2 = count.split(")")[0]
+    return account, count2
+
 
 
 ###################### Script ####################
 
 queue_filter()
-vivaweb_check()
+ime, nomer = mail_strip(fst)
+ime3, nomer3 = mail_strip(scd)
+try:
+    ime2, nomer2 = acc_strip(fst2)
+    ime4, nomer4 = acc_strip(scd2)
+    print ime2 + "   ====>  " + nomer2
+    print ime4 + "   ====>  " + nomer4
+    print ""
+except NameError:
+    print ""
+
+test = vivaweb_check(ime)
+test2 = vivaweb_check(ime3)
+if test:
+    print "Check the following account..   " + test
+    print ""
+    print "grep cwd /var/log/exim_mainlog|grep -v /var/spool|awk -F\"cwd=\" '{print $2}'|awk '{print $1}'|sort|uniq -c|sort -n | grep username"
+if test2:
+    print "Check the following account..   " + test2
+
+print ""
+print ""
+print ime + "   ====>   " + nomer
+print ime3 + "    ====>  " + nomer3
+print ""
+
