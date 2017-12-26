f1ac9721c5c933fc3d540f9dc9fc09674a274136
Viktor Georgiev <v.georgiev90@abv.bg>
2017-12-27 00:06:59 +0200

    Update hide-pass.py

--- hide-pass.py
+++ hide-pass.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python
-#Simple and basic password encoding
 
 import string
 import collections
-import sys
+import argparse
 import getpass
+
+
+###### Parser ##########
+
+parser = argparse.ArgumentParser(description='Basic password encoding based on digit key')
+parser.add_argument('--encode','-e',type=str, help='Provide the string to be encoded')
+parser.add_argument('--decode','-d',type=str, help='Provide encoded string to decode')
+parser.add_argument('--file','-f',type=str, help='File to search for password')
+parser.add_argument('--version','-v',action='version', version="%(prog)s 2.0\nCode may be up to 10 chars only digits..\nString must be without spaces...")
+args = parser.parse_args()
 
 ##### Variables #########
 
 chars = string.ascii_letters + string.digits + string.punctuation
 temp = collections.OrderedDict()
 
-####### Functions ########
+###### Functions ########
 
 def init():
-    print "Code may be up to 10 chars only digits.."
-    print "String must be without spaces..."
     code = getpass.getpass('Enter code: ')
-    parola = getpass.getpass('Enter the pass: ')
     count = 0
     for i in chars:
         temp[i] = count
         count = count + 1
-    return parola,code
+    return code
 
 def code_check(code):
     total = 0
     for i in code:
         total = total + int(i)
     return total
-
 
 def encode(parola,code):
     enc = []
@@ -62,22 +67,36 @@ def decode(enc_pass,code):
     dec_pass = ''.join(enc)
     return dec_pass
 
-####### Script ##########
+def file_act(file,search,replace):
+    search = "password:'%s'" % search
+    new_parola = "password:'%s'" % replace
+    with open(file,'r') as f:
+        cont = f.read()
+    cont = cont.replace(search,new_parola)
+    with open(file,'w') as f:
+        f.write(cont)
 
 
-try:
-    arg = sys.argv[1]
-    if arg == '--encode':
-        parola,code = init()
-        total = code_check(code)
-        pos = encode(parola,total)
-        print pos
-    elif arg == '--decode':
-        parola,code = init()
-        total = code_check(code)
-        pas = decode(parola,total)
-        print pas
-    else:
-        print "Usage:\n===================\n./hide.py --encode\n./hide.py --decode\n"
-except IndexError:
-    print "Usage:\n===================\n./hide.py --encode\n./hide.py --decode\n"
+
+########### Script #############
+
+
+if args.file and args.encode:
+    path = args.file
+    code = init()
+    total = code_check(code)
+    parola = args.encode
+    enc_pas = encode(parola,total)
+    file_act(path,parola,enc_pas)
+    print "Password encoded: %s" % enc_pas
+elif args.file and args.decode:
+    path = args.file
+    code = init()
+    total = code_check(code)
+    parola = args.decode
+    dec_pas = decode(parola,total)
+    file_act(path,parola,dec_pas)
+    print "Password decoded ready to use.."
+else:
+    print "To use -f option you must specify either decode or encode.."
+    print "hide.py --help for more info..."
