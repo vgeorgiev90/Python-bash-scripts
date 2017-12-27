06e94d522bc04a1fb10b4b153fec0e3e64f08ffe
Viktor Georgiev <v.georgiev90@abv.bg>
2017-12-27 16:34:25 +0200

    Update tomcat.py

--- tomcat.py
+++ tomcat.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
-#tomcat8 with java8 set up for multi user systems
-#included apache for vhost proxy
+##tomcat 8 and java 8 install and managment for multi user systems
+##Apache proxy virtual hosts creation for tomcat
 
-import sys
 import subprocess
 import random
 import string
+import argparse
 
-####### Functions ########
-
+######## Functions  ############
 
 def base_install():
     subprocess.call(["yum","install","java-1.8.0-openjdk.x86_64", "-y"])
@@ -20,11 +19,12 @@ def base_install():
     subprocess.call(["systemctl","enable","httpd"])
 
 
-def pw_gen(size = 12, chars=string.ascii_letters + string.digits + string.punctuation):
+def pw_gen(size = 12, chars=string.ascii_letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
 def multi_user_install(user):
+
 ##### Instance Variables #######
     port = random.randint(8000,9000)
     parola = pw_gen(20)
@@ -35,7 +35,7 @@ def multi_user_install(user):
     home = "/opt/tomcat-" + user
     own = user + ':'
     shutdown_pass = '<user username="%s" password="%s" roles="admin-gui,manager-gui"/>' % (user,parola)
-#### Make sure that there are no port conflicts ####
+#### Make sure that there are no port conflicts ##
     rep = 'port="8080"'
     ssl = 'redirectPort="8443"'
     shp = 'port="8005"'
@@ -78,15 +78,18 @@ def multi_user_install(user):
     filedata = filedata.replace(shut,shut2)
     with open(home + "/conf/server.xml", 'w') as file:
         file.write(filedata)
+    file.close()
     with open(home + '/conf/tomcat-users.xml','r') as file:
         cont = file.readlines()
     cont.insert(43,shutdown_pass)
     with open(home + '/conf/tomcat-users.xml','w') as file:
         for line in cont:
             file.write(line)
+
     data = "Instance created with home directory: %s \n \n User: %s\n Port: %s\n SSlPort: %s\n SHUTPort: %s\n AJPport: %s \n SHUTpass: %s\n AdminGUI user: %s \n AdminGUI pass: %s \n \nJava memory options by default: \n Xms=128m\n Xmx=512m\n PermSize=32m\n MaxPermSize=64m\n -server\n -XX:+UseParallelGC \n -Dfile.encoding=utf-8 \n If you want to increase or change modify the service file \n /usr/lib/systemd/system/tomcat-%s.service\n" % (home,user,port,sslport,shport,ajport,parola,user,parola,user)
     with open(home + "/tomi.info", 'w') as temp:
         temp.write(data)
+    temp.close()
     print "Instance is created with home directory: %s" % home
     print "For info on ports check %s/tomi.info" % home
     return user,home
@@ -116,6 +119,7 @@ def make_user_service(user,home):
     service = '/usr/lib/systemd/system/tomcat-' + user + ".service"
     with open(service,'w') as file:
         file.write(content)
+    file.close()
     subprocess.call(["systemctl","enable","tomcat-" + user])
     subprocess.call(["systemctl","start","tomcat-" + user])
     print "Systemd service file created for tomcat-%s.service" % user
@@ -135,39 +139,37 @@ def httpd_proxy(domain,install_port):
 
     with open(proxy_file,'w') as prfile:
         prfile.write(proxy_set)
+    prfile.close()
     subprocess.call(["systemctl","restart","httpd"])
     print "Proxy Virtual Host is created for the domain %s \n if you want to point it to spesific context on this instance \n modify %s \n as follows: \n ProxyPass / http://127.0.0.1:your-port/your-context/" % (domain,proxy_file)
 
 
-def usage():
-    subprocess.call(["clear"])
-    print "Usage for the scirpt"
-    print "================================================="
-    print "--base-install -- setup the base java8 and tomcat"
-    print "--user-install -- setup user specific instance    "
-    print "--vhost -- create proxy vhost for apache redirect"
+######### Argument Parser Declare ##############
 
-############# Script Start ################
+parser = argparse.ArgumentParser(description='Apache tomcat 8 setup and management script')
+parser.add_argument('--base_install','-b',action='store_true',help='Base install for java8 and tomcat8')
+parser.add_argument('--user_install','-u',type=str,help='User instance install, provide username')
+parser.add_argument('--vhost','-V',type=str,help='Install apache vhost, provide domain name and port for the user instance')
+parser.add_argument('--port','-p',type=int,help='Port for the vhost install, this option can be used only with -V')
+parser.add_argument('--version','-v',action='version',version='tomcat.py v2.0 for any bugs contact the developer of the script')
+args = parser.parse_args()
 
-try:
-    arg = sys.argv[1]
-    if arg == '--base-install':
-        base_install()
-    elif arg == '--user-install':
-        try:
-            usr = sys.argv[2]
-            acc,hom = multi_user_install(usr)
-            make_user_service(acc,hom)
-        except IndexError:
-            print "Usage: \n tomcat.py --user-install John"
-    elif arg == '--vhost':
-        try:
-            dom = sys.argv[2]
-            por = sys.argv[3]
-            httpd_proxy(dom,por)
-        except IndexError:
-            print "Usage: \n tomcat.py --vhost example.com 8080"
+######### Script Start ###############
+
+if args.base_install:
+    base_install()
+elif args.user_install:
+    usr = args.user_install
+    acc,hom = multi_user_install(usr)
+    make_user_service(acc,hom)
+elif args.vhost:
+    if args.port:
+        dom = args.vhost
+        por = args.port
+        httpd_proxy(dom,por)
     else:
-        usage()
-except IndexError:
-    usage()
+        print "Please provide port for the vhost with option --port or -p"
+elif args.port:
+    print "This option can be used only when you declare virtual host for apache..\ntomcat.py --help for more info."
+else:
+    print "Please provide valid options or try --help for more information."
