646097cb7d84504d5bc8fb0c09a416f8b167fef1
Viktor Georgiev <v.georgiev90@abv.bg>
2017-12-28 12:38:21 +0200

    Update basic-encrypt-module-functions.py

--- basic-encrypt-module-functions.py
+++ basic-encrypt-module-functions.py
@@ -54,14 +54,15 @@ def decode(enc_pass,code):
     dec_pass = ''.join(enc)
     return dec_pass
 
-def file_code(file,mode,code):   ## Code can be either 'encode' or 'decode' 
+def file_code(file,mode,code):                     ## Code can be either 'encode' or 'decode' 
     if mode == 'encode':
         with open(file,'r') as f:
             cont = f.read()
         content = cont.replace(' ','---')
         content2 = content.replace('\n','___')
-        new_data = content2
-        for i in range(1,10):    ## For stronger encoding use more iterations
+        content3 = content2.replace('\t','===')
+        new_data = content3
+        for i in range(1,10):                      ## For stronger encoding use more iterations
             old_data = encode(new_data,code)
             new_data = old_data
         with open(file,'w') as f:
@@ -70,11 +71,15 @@ def file_code(file,mode,code):   ## Code can be either 'encode' or 'decode'
         with open(file,'r') as f:
             cont = f.read()
         new_data = cont
-        for i in range(1,10):    ## Also change them for decoding the file
+        for i in range(1,10):                      ## Also change them for decoding the file
             old_data = decode(new_data,code)
             new_data = old_data
         content2 = new_data.replace('---',' ')
         content3 = content2.replace('___','\n')
+        content4 = content3.replace('===','\t')
         with open(file,'w') as f:
-            f.write(content3)
+            f.write(content4)
+    else:
+        print "The functions takes only encode or decode as argument"
+
 
