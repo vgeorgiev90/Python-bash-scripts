f1c75da26f8fe41f10d04f4fb850007fea400b46
Viktor Georgiev <v.georgiev90@abv.bg>
2018-11-22 16:01:28 +0200

    Update cf-zone-firewall.py

--- py/app/cf-zone-firewall.py
+++ py/app/cf-zone-firewall.py
@@ -10,12 +10,12 @@ key = "your-cf-key"                                                        #### 
 url1 = "https://api.cloudflare.com/client/v4/zones/9eeff1b8950ac2697e66560e19a199b5/firewall/lockdowns?&per_page=20"
 url2 = "https://api.cloudflare.com/client/v4/zones/9eeff1b8950ac2697e66560e19a199b5/firewall/lockdowns"
 
-
+### headers for curl
 h1 = "\"X-Auth-Email: %s\"" % mail
 h2 = "\"X-Auth-Key: %s\"" % key
 h3 = "\"Content-type: application/json\""
 
-
+### headers dict for requests
 headers = {
    "X-Auth-Email": mail,
    "X-Auth-Key": key ,
@@ -29,10 +29,7 @@ def Parser():
     parser.add_argument('--data','-d',nargs='+',help='User instance install, provide username')
     return parser
 
-
-
 class firewall():
-
     def __init__(self):
         self.response = requests.get(url1, headers=headers)
         self.dict = json.loads(self.response.text)
@@ -49,7 +46,6 @@ class firewall():
                 print ip['value']
 
     def create_rule(self, name):
-
         cmd = "curl -XPOST %s -H %s -H %s -H %s --data '{\"paused\": false, \"configurations\": [{\"target\":\"ip\",\"value\":\"192.168.1.1\"}] , \"urls\": [\"admim.finte.co/*\",\"admin.finte.co/*\"], \"description\": \"%s\" }'" % (url2, h1, h2, h3, name)
         response = os.system(cmd)
         print response
@@ -61,23 +57,18 @@ class firewall():
             if dict['value'] == ip:
                 print "%s is already in the list %s" % (ip, config['result']['description'])
                 raise SystemExit
-
         data = config['result']
         del data['id']
         data['configurations'].append({ "target": "ip", "value": ip})
-
-
         rep = str(data['configurations'])
         rep2 =  rep.replace("u'", "'")
         confs = rep2.replace("'", "\"")
-
         lock_url = url2 + '/' + rule_id
         cmd = "curl -XPUT %s -H %s -H %s -H %s --data '{\"paused\": false, \"configurations\": %s , \"urls\": [\"admim.finte.co/*\",\"admin.finte.co/*\"], \"description\": \"%s\" }'" % (lock_url, h1, h2, h3, confs, data['description'])
         response = os.system(cmd)
         print response
 
-
-
+############# Script ##################
 parser = Parser()
 args = parser.parse_args()
 
@@ -98,13 +89,11 @@ elif args.cmd == 'add-ip' and args.data:
 else:
     print """
         Sub-commands:
-
         list        ---  List all rules and associated IPs
         create-rule ---  Create new rule in firewall zone
         add-ip      ---  Add Ip address to existing rule
 
         Usage:
-
         cf-firewall.py list
         cf-firewall.py create-rule -d NAME
         cf-firewall.py add-ip -d RULE-ID IP-ADDRESS
