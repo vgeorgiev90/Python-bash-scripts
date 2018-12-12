1a623b6ea639729c2b398b0ed74fd4a6346d2766
Viktor Georgiev <v.georgiev90@abv.bg>
2018-12-12 12:01:39 +0200

    Update check.py

--- py/util/k8s/check.py
+++ py/util/k8s/check.py
@@ -12,11 +12,10 @@ def Parser():
     return parser
 
 
-
 class check():
-    api_address='https://10.0.11.155:6443'
-    headers =  { "Authorization" : "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJrdWJlLXN5c3RlbSIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJ2aWt0b3ItdG9rZW4tZzJnNDYiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoidmlrdG9yIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiOTlkZGYxZTItZmEyNi0xMWU4LTkyMWUtYTJjNjBhNTMzNWJlIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Omt1YmUtc3lzdGVtOnZpa3RvciJ9.FK9YEFFv1NLmOMaqn8ksaO9rNCX6t6azA6DldHB79WuAYGNl_5OsaM6mqjFYeTvUF229AImqGzltBRjTIm05AOIZKnK-yKn5SpYLTV1TfLMW4ZGrZzy0QQWHPmn3_XFOWCfHeTaDu8iJQsknAm_1TzwERblX_FpapzuZ3CtNo8AEQ0EemoD9_gQvzFC2ghuhazs89xYkZt38yr2djTWLoFjfeL4kK_k1tKo1Lv-5p2Lc4ieCbiI5K-1VEC6Dtrvv2vR-43AgX71Ely_OQE5D5bc1NyufgUgwfWwAfKgI4_fMw3EkCUs4jurxWQOe9zVW4NWbUU3TH2hXZZhS-vh6Sg" }
-    ca_cert="ca-cert"
+    api_address='https://KUBE_API:KUBE_API_PORT'
+    headers =  { "Authorization" : "Bearer YOUR_TOKEN_HERE" }
+    ca_cert="path/to/cluster/ca-certificate"
 
     def __init__(self):
         self.namespaces = []
@@ -46,12 +45,14 @@ class check():
     def proc(self,pod):
         p = pod.split("/")[1]
         ns = pod.split("/")[0]
+
         ## Find docker container id and host for the pod
         pod_url = '%s/api/v1/namespaces/%s/pods/%s' % (self.api_address, ns, p)
         response = requests.get(pod_url, headers=self.headers, verify=self.ca_cert )
         response = json.loads(response.text)
         host_ip = response['status']['hostIP']
         cont_id = response['status']['containerStatuses'][0]['containerID'].split("//")[1]
+
         ## Process checking depends on ssh connection to the host so passwordless login must be setup.
         port = '22'
         username = 'root'
