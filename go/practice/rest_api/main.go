445dd679741c21da8735a4a1a102800a81a26bc4
vgeorgiev90 <v.georgiev90@abv.bg>
2019-03-06 15:49:49 +0200

    rest

---
+++ go/practice/rest_api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"rest_api/functions"
+)
+
+func main() {
+	//functions.Db_install("10.0.11.165", "viktor123")
+	token := functions.Get_token("10.0.11.165", "ioana")
+	//functions.Create_token("10.0.11.165", "ioana")
+}
