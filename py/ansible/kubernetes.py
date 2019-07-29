abd4a4a8615fd2cc5d747bef5176d5841974ce53
Viktor Georgiev <v.georgiev90@abv.bg>
2019-07-29 10:48:35 +0300

    Update kubernetes.py

--- py/ansible/kubernetes.py
+++ py/ansible/kubernetes.py
@@ -25,6 +25,7 @@ Notes:
 There are two ways to provide object data for the moment: kubernetes yaml definition file, or json formatted data
 """
 
+
 EXAMPLES = """
 #Create deployment from yaml file
 
@@ -59,211 +60,136 @@ EXAMPLES = """
 
 """
 
-### Namespace resource ######################
-
-def namespace_present(data):
-    config = data
-    api_address = config['api_address']
-    token = config['token']
-    headers = {
-        "Authorization": "",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    headers['Authorization'] = "Bearer " + token
-
-    url = api_address + "/api/v1/namespaces"
-    if config.get('data'):
-        obj_data = json.loads(config['data'])
-    elif config.get('file'):
-        file_path = config['file']
-        with open(file_path, 'r') as f:
-            obj = f.read()
-        obj_data = json.loads(json.dumps(yaml.safe_load(obj)))
-    else:
-        has_changed = False
-        is_error = True
-        meta = "Please provide eith data field with json formated kubernetes data or file with path to the yaml file"
-        return (has_changed, meta, is_error)
-    requests.packages.urllib3.disable_warnings()
-    response = requests.post(url, headers=headers, data=json.dumps(obj_data), verify=False)
-    if response.status_code == 201:
-        has_changed = True
-        meta = response.json()
-        is_error = False
-        return (has_changed, meta, is_error)
-    else:
-        has_changed = False
-        meta = response.json()
-        is_error = True
-        return (has_changed, meta, is_error)
-
-def namespace_absent(data):
-    config = data
-    api_address = config['api_address']
-    token = config['token']
-    headers = {
-        "Authorization": "",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    headers['Authorization'] = "Bearer " + token
-    if config.get('name'):
-        obj_name = config['name']
-    else:
-        has_changed = False
-        meta = "Please provide object name for absent state"
-        is_error = True
-        return (has_changed, meta, is_error)
-
-    url = api_address + "/api/v1/namespaces/" + obj_name
-    requests.packages.urllib3.disable_warnings()
-    response = requests.delete(url, headers=headers, verify=False)
-    if response.status_code == 201 or response.status_code == 200:
-        has_changed = True
-        meta = response.json()
-        is_error = False
-        return (has_changed, meta, is_error)
-    else:
-        has_changed = False
-        meta = response.json()
-        is_error = True
-        return (has_changed, meta, is_error)
 
 
-#### Deployment resource functions ############
-def present(data):
-    config = data
-    api_address = config['api_address']
-    token = config['token']
-    kind = data['type']
 
-    headers = {
-        "Authorization": "",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    headers['Authorization'] = "Bearer " + token
+class connection():
 
-    ## Check if inline deployment data is provided or yaml file is referenced and extract the namespace as it will be needed for url construction
-    if config.get('data'):
-        obj_data = json.loads(config['data'])
-        if obj_data('namespace'):
-            namespace = obj_data['namespace']
+    def __init__(self, data):
+        self.read_endpoints = {
+                "deployment": "/apis/extensions/v1beta1/deployments",
+                "configmap": "/api/v1/configmaps/",
+                "secret": "/api/v1/secrets/",
+                "service": "/api/v1/services/",
+                "persistentvolumeclaim": "/api/v1/persistentvolumeclaims/",
+                "statefulset": "/apis/apps/v1/statefulsets",
+                "namespace": "/api/v1/namespaces/",
+        }
+
+        self.write_endpoints = {
+                "deployment": "/apis/apps/v1/namespaces/",
+                "configmap": "/api/v1/namespaces/",
+                "secret": "/api/v1/namespaces/",
+                "service": "/api/v1/namespaces/",
+                "persistentvolumeclaim": "/api/v1/namespaces/",
+                "statefulset": "/apis/apps/v1/namespaces/",
+                "namespace": "/api/v1/namespaces"
+        }
+        self.headers = {
+                "Authorization": "",
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+        }
+        self.api_address = data['api_address']
+        self.token = data['token']
+        self.kind = data['type']
+        self.state = data['state']
+        self.headers['Authorization'] = "Bearer " + self.token
+
+
+    def present(self, data):
+
+        if data.get('data'):
+            self.obj_data = json.loads(data['data'])
+            if data['type'] != 'namespace':
+                if self.obj_data('metadata')['namespace']:
+                    self.namespace = self.obj_data['metadata']['namespace']
+                else:
+                    self.namespace = "default"
+        elif data.get('file'):
+            file_path = data['file']
+            with open(file_path, 'r') as f:
+                obj = f.read()
+            self.obj_data = json.loads(json.dumps(yaml.safe_load(obj)))
+            if data['type'] != 'namespace':
+                if self.obj_data.get('metadata')['namespace']:
+                    self.namespace = self.obj_data['metadata']['namespace']
+                else:
+                    self.namespace = "default"
         else:
-            namespace = "default"
-    elif config.get('file'):
-        file_path = config['file']
-        with open(file_path, 'r') as f:
-            obj = f.read()
-        obj_data = json.loads(json.dumps(yaml.safe_load(obj)))
-        if obj_data.get('namespace'):
-            namespace = obj_data['namespace']
-        else:
-            namespace = "default"
-    else:
-        has_changed = False
-        is_error = True
-        meta = "Please provide eith data field with json formated kubernetes data or file with path to the yaml file"
-        return (has_changed, meta, is_error)
-
-    endpoints = {
-         "deployment": "/apis/apps/v1/namespaces/",
-         "configmap": "/api/v1/namespaces/",
-         "secret": "/api/v1/namespaces/",
-         "service": "/api/v1/namespaces/",
-         "persistentvolumeclaim": "/api/v1/namespaces/",
-         "statefulset": "/apis/apps/v1/namespaces/"
-    }
+            has_changed = False
+            is_error = True
+            meta = "Please provide eith data field with json formated kubernetes data or file with path to the yaml file"
+            return (has_changed, meta, is_error)
 
 
-    ## Construct the url and submit
-    if kind == "deployment":
-        url = api_address + endpoints[kind] + namespace + "/deployments"
-    elif kind == "configmap":
-        url = api_address + endpoints[kind] + namespace + "/configmaps"
-    elif kind == "secret":
-        url = api_address + endpoints[kind] + namespace + "/secrets"
-    elif kind == "service":
-        url = api_address + endpoints[kind] + namespace + "/services"
-    elif kind == "persistentvolumeclaim":
-        url = api_address + endpoints[kind] + namespace + "/persistentvolumeclaims"
-    elif kind == "statefulset":
-        url = api_address + endpoints[kind] + namespace + "/statefulsets"
+        if self.kind == "deployment":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/deployments"
+        elif self.kind == "configmap":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/configmaps"
+        elif self.kind == "secret":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/secrets"
+        elif self.kind == "service":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/services"
+        elif self.kind == "persistentvolumeclaim":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/persistentvolumeclaims"
+        elif self.kind == "statefulset":
+            url = self.api_address + self.write_endpoints[self.kind] + self.namespace + "/statefulsets"
+        elif self.kind == "namespace":
+            url = self.api_address + self.write_endpoints[self.kind]
 
-    requests.packages.urllib3.disable_warnings()
-    response = requests.post(url, headers=headers, data=json.dumps(obj_data), verify=False)
+        requests.packages.urllib3.disable_warnings()
+        response = requests.post(url, headers=self.headers, data=json.dumps(self.obj_data), verify=False)
 
-    ## Check the status code to determine success or failure
-    if response.status_code == 201 or response.status_code == 200:
-        has_changed = True
-        meta = response.json()
-        is_error = False
-        return (has_changed, meta, is_error)
-    else:
-        has_changed = False
-        meta = response.json()
-        is_error = True
-        return (has_changed, meta, is_error)
-
-
-def absent(data):
-    config = data
-    api_address = config['api_address']
-    token = config['token']
-    kind = config['type']
-
-    headers = {
-        "Authorization": "",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    headers['Authorization'] = "Bearer " + token
-
-    ##  Check if deployment name is provided or not to construct all deployments check url
-    if config.get('name'):
-        obj_name = config['name']
-    else:
-        has_changed = False
-        meta = "Please provide object name for absent state"
-        is_error = True
-        return (has_changed, meta, is_error)
-
-    ## Api endpoints based on object type
-    endpoints = {
-        "deployment": "/apis/extensions/v1beta1/deployments",
-        "configmap": "/api/v1/configmaps/",
-        "secret": "/api/v1/secrets/",
-        "service": "/api/v1/services/",
-        "persistentvolumeclaim": "/api/v1/persistentvolumeclaims/",
-        "statefulset": "/apis/apps/v1/statefulsets",
-    }
-
-    ## Check all objects of this type in the cluster the match the correct one and extract the selfLink which will be used to construct delete url
-    check_url = api_address + endpoints[kind]
-    requests.packages.urllib3.disable_warnings()
-    data = requests.get(check_url, headers=headers ,verify=False)
-
-    all_objects = data.json()
-    ## Extract the delete url for the specified deployment
-    for item in all_objects['items']:
-        name = item['metadata']['name']
-        url = api_address + item['metadata']['selfLink']
-        if name == obj_name:
-            if kind == 'deployment' or kind == 'statefulset':
-                delete = requests.delete(url + "?propagationPolicy=Foreground", headers=headers, verify=False)
-            else:
-                delete = requests.delete(url, headers=headers, verify=False)
+        ## Check the status code to determine success or failure
+        if response.status_code == 201 or response.status_code == 200:
             has_changed = True
-            meta = delete.json()
+            meta = response.json()
             is_error = False
             return (has_changed, meta, is_error)
         else:
             has_changed = False
-            meta = "Object with such name not found.."
+            meta = response.json()
             is_error = True
             return (has_changed, meta, is_error)
-############################################################################
+
+
+    def absent(self, data):
+        if data.get('name'):
+            self.obj_name = data['name']
+        else:
+            has_changed = False
+            meta = "Please provide object name for absent state"
+            is_error = True
+            return (has_changed, meta, is_error)
+        check_url = self.api_address + self.read_endpoints[self.kind]
+        requests.packages.urllib3.disable_warnings()
+        reply = requests.get(check_url, headers=self.headers ,verify=False)
+
+
+        all_objects = reply.json()
+        ## Extract the delete url for the specified deployment
+        for item in all_objects['items']:
+            name = item['metadata']['name']
+
+            url = self.api_address + item['metadata']['selfLink']
+            if name == self.obj_name:
+                if self.kind == 'deployment' or self.kind == 'statefulset':
+                    delete = requests.delete(url + "?propagationPolicy=Foreground", headers=self.headers, verify=False)
+                else:
+                    delete = requests.delete(url, headers=self.headers, verify=False)
+                has_changed = True
+                meta = delete.json()
+                is_error = False
+                return (has_changed, meta, is_error)
+
+        has_changed = False
+        meta = "Object with name: {} not found..".format(self.obj_name)
+        is_error = True
+        return (has_changed, meta, is_error)
+
+
+
 
 
 
@@ -288,40 +214,43 @@ def main():
         "name": {"required": False, "type": "str"},
     }
 
+    module = AnsibleModule(argument_spec=arguments)
+    con = connection(module.params)
+
     ### Choose function based on type and state declared
     choice_map = {
         "deployment": {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
          },
         "configmap": {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
          },
         "secret" : {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
         },
         "service" : {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
         },
         "persistentvolumeclaim" : {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
         },
         "statefulset" : {
-                "present": present,
-                "absent": absent,
+                "present": con.present,
+                "absent": con.absent,
         },
         "namespace" : {
-                "present": namespace_present,
-                "absent": namespace_absent,
+                "present": con.present,
+                "absent": con.absent,
         },
 
     }
 
-    module = AnsibleModule(argument_spec=arguments)
+
     has_changed, result, is_error = choice_map.get(module.params['type'])[module.params['state']](module.params)
     ### Check the execution status
     if is_error == False:
